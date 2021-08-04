1b6e180dd8de162b058b311e3ee8de345871001e
lixworth <lixworth@gmail.com>
2021-08-04 14:14:37 +0800

    frontend project init (test)

---
+++ frontend/src/Install.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    2333
+    <div class="alert alert-primary" role="alert">
+      A simple primary alert—check it out!
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Install"
+}
+</script>
+
+<style scoped>
+@import "~bootstrap/dist/css/bootstrap.min.css";
+</style>
---
+++ frontend/vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    pages :{
+        install: {
+            entry: 'src/install.js',
+            template: 'public/install.html',
+        },
+        index: {
+            entry: 'src/main.js',
+            template: 'public/index.html',
+        },
+    }
+};
